01b9b9c13cb7ba8435a18d855362b85f3e7ad604
FratNightGaming <jberger1@iwu.edu>
2022-10-31 01:26:35 -0500

    initial commit

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using CoffeeShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        CoffeeShopProductsContext db = new CoffeeShopProductsContext();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Detail(int ID)
+        {
+            List<Product> products= db.Products.ToList();
+            return View(products[ID-1]);
+        }
+    }
+}
